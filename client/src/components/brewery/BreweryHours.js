5dfb994d8abf2ab2883522998669aca3c811f0f5
Dave Regg <drregg6@gmail.com>
2019-07-12 17:41:59 -0400

    BreweryHours shows CLOSED or HOURS based on hours.available

--- client/src/components/brewery/BreweryHours.js
+++ client/src/components/brewery/BreweryHours.js
@@ -2,19 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const BreweryHours = ({
-  hours: {
-    monIsOpen,
-    tuesIsOpen,
-    wedIsOpen,
-    thursIsOpen,
-    friIsOpen,
-    satIsOpen,
-    sunIsOpen
-  }
+  hours
 }) => {
   return (
     <div>
-      {monIsOpen}
+      <ul>
+        <li>
+          Monday: {hours.monIsOpen ? (
+            `${hours.monOpen} - ${hours.monClose}`
+          ) : ('Closed')}
+        </li>
+        <li>
+          Tuesday: {hours.tuesIsOpen ? (
+            `${hours.tuesOpen} - ${hours.tuesClose}`
+          ) : ('Closed')}
+        </li>
+        <li>
+          Wednesday: {hours.wedIsOpen ? (
+            `${hours.wedOpen} - ${hours.wedClose}`
+          ) : ('Closed')}
+        </li>
+        <li>
+          Thursday: {hours.thursIsOpen ? (
+            `${hours.thursOpen} - ${hours.thursClose}`
+          ) : ('Closed')}
+        </li>
+        <li>
+          Friday: {hours.friIsOpen ? (
+            `${hours.friOpen} - ${hours.friClose}`
+          ) : ('Closed')}
+        </li>
+        <li>
+          Saturday: {hours.satIsOpen ? (
+            `${hours.satOpen} - ${hours.satClose}`
+          ) : ('Closed')}
+        </li>
+        <li>
+          Sunday: {hours.sunIsOpen ? (
+            `${hours.sunOpen} - ${hours.sunClose}`
+          ) : ('Closed')}
+        </li>
+      </ul>
     </div>
   )
 }
