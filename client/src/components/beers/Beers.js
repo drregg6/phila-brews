b282a6bbfac9eeee9734077e4d6a51119fa5531f
Dave Regg <drregg6@gmail.com>
2019-07-16 18:42:30 -0400

    Beers component created

---
+++ client/src/components/beers/Beers.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+
+const Beers = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Beers.propTypes = {
+
+}
+
+export default connect(
+  null,
+  {}
+)(Beers);
