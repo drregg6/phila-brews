9121ff267acfea6dcf9333b77e87700c84ac76dc
Dave Regg <drregg6@gmail.com>
2019-07-28 19:34:56 -0400

    Server set

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Connect to db
 connectDB();
@@ -13,6 +14,15 @@ app.use('/api/breweries', require('./routes/api/breweries'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/contact', require('./routes/api/contact'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
