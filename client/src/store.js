4c59ca562aaf55c98c00a3c64e1b0b4d6a01aacf
drregg6 <dave.r.regg@gmail.com>
2023-07-05 13:42:04 -0400

    Rearranged and renamed Component files, modernized redux store and started building redux slices and services

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,27 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
 
-import rootReducer from './reducers';
+// import rootReducer from './reducers';
 
-const initialState = {};
-const middleware = [thunk];
+// const initialState = {};
+// const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
 
-export default store;
+import { configureStore } from '@reduxjs/toolkit';
+import breweryReducer from './features/brewery/brewerySlice';
+import alertReducer from './features/alert/alertSlice';
+import authReducer from './features/auth/authSlice';
+
+export default configureStore({
+  reducer: {
+    brewery: breweryReducer,
+    alert: alertReducer,
+    auth: authReducer
+  }
+});
